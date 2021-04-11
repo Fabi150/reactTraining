e33bcf7ea5bada5e6bbab97d09fc35fbf13ce11c
fabi150 <r.fabi.f@gmail.com>
2021-04-11 17:16:36 +0200

    init 3

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,8 +1,7 @@
 import react from 'react';
-import DanAbramovImage from 'https://ekino-tv.pl/static/thumb/qtnun0lczts4.jpg';
 const ListItem = () => (
     <li className='listItemWrapper'>
-        <img src={DanAbramovImage}/>
+
         <div>
             <h2>Dan Abramov</h2>
             <p>
